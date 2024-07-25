282d57cab6d1fba0f2bfc12b1c11d2484c7cc7f0
Alwin Helor <alwin24012002@gmail.com>
2024-07-25 12:22:26 +0530

    adding listener on tokenMint

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { Connection, PublicKey } from "@solana/web3.js";
 import { configDotenv } from "dotenv";
 import { Telegraf } from "telegraf";
-import connectToDatabase from "./utils/database";
 import Token from "./models/token";
-import listener from "./utils/listener";
+import connectToDatabase from "./utils/database";
+import callback from "./utils/listenerCallback";
 configDotenv();
 
 export const bot = new Telegraf(process.env.BOT_TOKEN!);
+export const connection = new Connection(process.env.BACKEND_RPC!);
 
-// Respond to /start command
+export const subcriptionIds: Record<string, number> = {};
+
 bot.start((ctx) => {
   if (ctx.chat.type === "private") {
     ctx.reply("Welcome to Whale Notifier Bot!");
   } else if (ctx.chat.type === "group" || ctx.chat.type === "supergroup") {
     ctx.reply("Whale Notifier Bot has been added to this group!");
-  }
-});
-
-bot.command("gm", (ctx) => {
-  if (ctx.chat.type === "group" || ctx.chat.type === "supergroup") {
-    ctx.reply("Good Morning!");
   }
 });
 
@@ -31,11 +29,22 @@ bot.command("register", async (ctx) => {
     return;
   }
 
-  const [command, token, minValueStr] = params;
+  const [command, tokenMint, minValueStr] = params;
   const minValue = Number(minValueStr);
 
   if (isNaN(minValue)) {
     await ctx.reply("Please provide a valid number for the minimum value.");
+    return;
+  }
+
+  try {
+    const accInfo = await connection.getAccountInfo(new PublicKey(tokenMint));
+    if (!accInfo?.owner.equals(TOKEN_PROGRAM_ID)) {
+      await ctx.reply("Please provide a valid token mint address.");
+      return;
+    }
+  } catch (error: any) {
+    await ctx.reply(error.message);
     return;
   }
 
@@ -46,16 +55,22 @@ bot.command("register", async (ctx) => {
   try {
     await Token.create({
       groupId,
-      tokenMint: token,
+      tokenMint,
       minValue,
     });
+
+    subcriptionIds[tokenMint] = connection.onLogs(
+      new PublicKey(tokenMint),
+      callback
+    );
+
     await ctx.reply(
-      `Registered token: ${token}, with minimum value: ${minValue}`
+      `Registered token: ${tokenMint}, with minimum value: ${minValue}`
     );
   } catch (err: any) {
     if (err.code === 11000) {
       await ctx.reply(
-        `Token: ${token} has already been registered for this group.`
+        `Token: ${tokenMint} has already been registered for this group.`
       );
     } else {
       await ctx.reply("An error occurred while registering the token.");
@@ -64,12 +79,57 @@ bot.command("register", async (ctx) => {
   }
 });
 
+bot.command("unregister", async (ctx) => {
+  const messageText = ctx.message.text;
+  const params = messageText.split(" ");
+
+  if (params.length !== 2) {
+    await ctx.reply("Usage: /unregister <token_mint>");
+    return;
+  }
+
+  const [command, tokenMint] = params;
+
+  try {
+    await connectToDatabase();
+    const result = await Token.deleteOne({
+      groupId: ctx.chat.id,
+      tokenMint,
+    });
+
+    connection.removeOnLogsListener(subcriptionIds[tokenMint]);
+
+    if (result.deletedCount === 0) {
+      await ctx.reply(
+        `Token: ${tokenMint} has not been registered for this group.`
+      );
+    } else {
+      await ctx.reply(`Unregistered token: ${tokenMint}`);
+    }
+  } catch (err: any) {
+    await ctx.reply("An error occurred while unregistering the token.");
+  }
+});
+
+const listener = async () => {
+  await connectToDatabase();
+
+  const listenedTokens = await Token.find().distinct("tokenMint");
+
+  console.log("Listening to tokens: ", listenedTokens);
+  for (let i = 0; i < listenedTokens.length; i++) {
+    const tokenMint = listenedTokens[i];
+    subcriptionIds[tokenMint] = connection.onLogs(
+      new PublicKey(tokenMint),
+      callback
+    );
+  }
+};
+
 listener();
 
-// Error handling
 bot.catch((err) => {
   console.error("Error occurred", err);
 });
 
-// Start the bot
 bot.launch().then(() => console.log("Bot started!"));
---
+++ src/utils/listenerCallback.ts
@@ -0,0 +1,74 @@
+import { Context, Logs } from "@solana/web3.js";
+import { bot, connection } from "..";
+import Token from "../models/token";
+import TxnSignature from "../models/txnSignature";
+
+const callback = async (logs: Logs, context: Context) => {
+  if (logs.err) return;
+
+  const txnSignature = logs.signature;
+  await TxnSignature.create({ txnSignature }).catch(() => {
+    return;
+  });
+
+  const info = await connection.getParsedTransaction(txnSignature, {
+    maxSupportedTransactionVersion: 0,
+  });
+
+  if (!info || !info.meta) return;
+
+  const preTokenBalances = info.meta.preTokenBalances;
+  const postTokenBalances = info.meta.postTokenBalances;
+
+  if (!preTokenBalances || !postTokenBalances) return;
+
+  const signer = info.transaction.message.accountKeys
+    .find((key) => key.signer)
+    ?.pubkey.toBase58();
+
+  const tokenChanges: Record<string, number> = {};
+
+  for (let i = 0; i < preTokenBalances.length; i++) {
+    const preTokenBalance = preTokenBalances[i];
+    const postTokenBalance = postTokenBalances[i];
+
+    if (!preTokenBalance || !postTokenBalance) continue;
+
+    if (preTokenBalance.owner !== signer) continue;
+
+    const mint = preTokenBalance.mint;
+
+    if (
+      preTokenBalance.uiTokenAmount.uiAmount !==
+      postTokenBalance.uiTokenAmount.uiAmount
+    ) {
+      tokenChanges[mint] = Math.abs(
+        postTokenBalance.uiTokenAmount.uiAmount! -
+          preTokenBalance.uiTokenAmount.uiAmount!
+      );
+    }
+  }
+
+  const listeningGroups = await Token.find({
+    tokenMint: { $in: Object.keys(tokenChanges) },
+  }).lean();
+
+  for (let i = 0; i < listeningGroups.length; i++) {
+    const listeningGroup = listeningGroups[i];
+    const tokenMint = listeningGroup.tokenMint;
+    const tokenChange = tokenChanges[tokenMint];
+
+    if (tokenChange < listeningGroup.minValue) {
+      continue;
+    }
+
+    await bot.telegram.sendMessage(
+      listeningGroup.groupId,
+      `Token: ${tokenMint} has changed by ${tokenChange.toFixed(
+        2
+      )} in transaction: ${txnSignature}`
+    );
+  }
+};
+
+export default callback;
